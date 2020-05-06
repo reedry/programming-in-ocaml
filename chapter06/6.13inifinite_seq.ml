baa18c4b9e3cdc7e08713e9128df77205e28c031
reedry <r.yoshida.c@gmail.com>
2020-05-07 00:12:23 +0900

    Rename fib_rec -> fib_seq

--- chapter06/6.13inifinite_seq.ml
+++ chapter06/6.13inifinite_seq.ml
@@ -3,8 +3,8 @@ type intseq = Cons of int * (int -> intseq)
 let rec f x = Cons (x + 1, f)
 let rec step n x = Cons (x + n, step (n + 1))
 
-let rec fib_rec n x = Cons (x + n, fib_rec x)
-let fib = fib_rec 1 0
+let rec fib_seq n x = Cons (x + n, fib_seq x)
+let fib = fib_seq 1 0
 
 let rec nthseq n (Cons (x, f)) =
   if n = 1 then x
